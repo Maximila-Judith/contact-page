ada0848a8a05e3d6f5b29ba6d9897a9612b07113
Maximila-Judith <houngbo.maximila@gmail.com>
2024-10-30 19:59:09 +0100

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,54 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col lg:items-center gap-y-10 lg:p-20">
+      <div className="flex flex-col items-center gap-y-8 p-10">
+        <h1 className="text-white font-bold text-4xl md:text-6xl text-center">Interested in our <br /> business pricing?</h1>
+        <p className="text-white text-xl font-bold text-center">Fill out the form to view details and we&rsquo;ll <br /> contact you as soon as possible.</p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="lg:w-3/4 flex flex-col gap-10 bg-[#CDD5E066] rounded-3xl m-10 p-10">
+        <div className=" items-center grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="">
+            <label className="block text-sm font-medium mb-2 text-white font-bold">Name</label>
+            <input className="py-6 px-10 pe-9 block w-full bg-gray-100 rounded-2xl text-md font-semibold dark:text-[#CDD5E0] text-opacity-10" placeholder="Ethan Johnson" />
+          </div>
+          <div className="">
+            <label className="block text-sm font-medium mb-2 text-white font-bold">Company Email</label>
+            <input className="py-6 px-10 pe-9 block w-full bg-gray-100 rounded-2xl text-md font-semibold dark:text-[#CDD5E0] text-opacity-10" placeholder="ethan@johnson.com" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className=" items-center grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div>
+            <label className="block text-sm font-medium mb-2 text-white font-bold">Company size</label>
+            <select className="py-6 px-5 block w-full rounded-2xl text-md font-semibold dark:text-black text-opacity-10" >
+              <option>50-100 employees</option>
+              <option>100-200 employees</option>
+              <option>200-300 employees</option>
+              <option>300-400 employees</option>
+            </select>
+          </div>
+          <div>
+            <label className="block text-sm font-medium mb-2 text-white font-bold">Subject</label>
+            <select className="py-6 px-5 block w-full rounded-2xl text-md font-semibold dark:text-black text-opacity-10" >
+              <option>Building Landing pages</option>
+              <option>Building web site</option>
+              <option>Building web app</option>
+              <option>Building mobile app</option>
+            </select>
+          </div>
+        </div>
+
+        <div>
+          <label className="block text-sm font-medium mb-2 text-white font-bold">Message</label>
+          <textarea id="hs-textarea-ex-2" className="p-4 pb-20 block w-full rounded-2xl text-md dark:text-black font-semibold dark:placeholder-black" placeholder="50-100 employees"></textarea>
+        </div>
+
+        <button type="button" className="w-full py-5 px-4 inline-flex justify-center items-center gap-x-2 text-xl font-bold rounded-2xl dark:bg-white dark:text-[#4036A9]">
+          Contact Sales
+        </button>
+      </div>
     </div>
   );
 }
